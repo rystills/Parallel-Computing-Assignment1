a69644b5a3a2cc340a08aa20fb19b36d9154dead
Ryan Stillings <rystills@gmail.com>
2019-01-27 04:48:04 -0500

    read hex data to strings, simple conversion to byte arrays

--- src/cla.c
+++ src/cla.c
@@ -27,14 +27,40 @@ int sspl[nsupersections] = {0} ;
 int sscl[nsupersections] = {0} ;
 int sumi[bits] = {0};
 //Integer array of inputs in binary form
-int* bin1=NULL;
-int* bin2=NULL;
+int bin1[bits];
+int bin2[bits];
 //Character array of inputs in hex form
-char* hex1=NULL;
-char* hex2=NULL;
+char hex1[digits];
+char hex2[digits];
 
 void s1() {
 
+}
+
+/**
+ * simple hex to binary conversion
+ * @param hex: a single hex value in character form (0-F)
+ * @param bin: an int array pointer at which to store the 4 calculated binary values
+ */
+void hexToBin(char hex, int* bin) {
+	switch (hex) {
+		case '0': *bin = 0; *(bin+1) = 0; *(bin+2) = 0; *(bin+3) = 0; break;
+		case '1': *bin = 0; *(bin+1) = 0; *(bin+2) = 0; *(bin+3) = 1; break;
+		case '2': *bin = 0; *(bin+1) = 0; *(bin+2) = 1; *(bin+3) = 0; break;
+		case '3': *bin = 0; *(bin+1) = 0; *(bin+2) = 1; *(bin+3) = 1; break;
+		case '4': *bin = 0; *(bin+1) = 1; *(bin+2) = 0; *(bin+3) = 0; break;
+		case '5': *bin = 0; *(bin+1) = 1; *(bin+2) = 0; *(bin+3) = 1; break;
+		case '6': *bin = 0; *(bin+1) = 1; *(bin+2) = 1; *(bin+3) = 0; break;
+		case '7': *bin = 0; *(bin+1) = 1; *(bin+2) = 1; *(bin+3) = 1; break;
+		case '8': *bin = 1; *(bin+1) = 0; *(bin+2) = 0; *(bin+3) = 0; break;
+		case '9': *bin = 1; *(bin+1) = 0; *(bin+2) = 0; *(bin+3) = 1; break;
+		case 'A': *bin = 1; *(bin+1) = 0; *(bin+2) = 1; *(bin+3) = 0; break;
+		case 'B': *bin = 1; *(bin+1) = 0; *(bin+2) = 1; *(bin+3) = 1; break;
+		case 'C': *bin = 1; *(bin+1) = 1; *(bin+2) = 0; *(bin+3) = 0; break;
+		case 'D': *bin = 1; *(bin+1) = 1; *(bin+2) = 0; *(bin+3) = 1; break;
+		case 'E': *bin = 1; *(bin+1) = 1; *(bin+2) = 1; *(bin+3) = 0; break;
+		case 'F': *bin = 1; *(bin+1) = 1; *(bin+2) = 1; *(bin+3) = 1; break;
+	}
 }
 
 void cla() {
@@ -42,10 +68,24 @@ void cla() {
 }
 
 void parseInput(char* fName) {
+	scanf("%s", hex1);
+	for (int i = 0; i < input_size; hexToBin(hex1[i],bin1+4*i), ++i);
 
+	for (int i = 0; i < input_size*4; ++i) {
+		printf("%d",bin1[i]);
+	}
+	puts("");
+
+	scanf("%s", hex2);
+	for (int i = 0; i < input_size; hexToBin(hex2[i],bin2+4*i), ++i);
+
+	for (int i = 0; i < input_size*4; ++i) {
+		printf("%d",bin2[i]);
+	}
+	puts("");
 }
 
 int main(int argc, char* argv) {
 	parseInput(argv[0]);
-
+	//cla();
 }
